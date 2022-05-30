855a42033694c57e831f563a2bdf2284d35cc080
JacksonSM <Jacksons.m2018@gmail.com>
2022-05-30 18:28:55 -0300

    Add project files.

---
+++ DESAFIO-SERIE-DIO-GFT-START-5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DESAFIO_SERIE_DIO_GFT_START_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
